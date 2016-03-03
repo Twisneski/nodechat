a8b702311c49871e8a1e1a81c1decd90c984d205
Twisneski <tina.wisneski525@gmail.com>
2016-03-03 09:58:44 -0600

    added server

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`)
+})
